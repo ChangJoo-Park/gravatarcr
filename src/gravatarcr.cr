c0027181e94298135625d97122f161f2872cbf7f
ChangJoo Park <pcjpcj2@gmail.com>
2018-02-02 18:14:10 +0900

    Initialize Commit

---
+++ src/gravatarcr.cr
@@ -0,0 +1,6 @@
+require "./gravatarcr/*"
+
+# TODO: Write documentation for `Gravatarcr`
+module Gravatarcr
+  # TODO: Put your code here
+end
---
+++ src/gravatarcr/version.cr
@@ -0,0 +1,3 @@
+module Gravatarcr
+  VERSION = "0.1.0"
+end
