1dcd34cc45800d0d2cf15c1559fc3a2fc283e60c
ChangJoo Park <pcjpcj2@gmail.com>
2018-02-02 18:52:17 +0900

    Release 0.1.0

--- spec/gravatarcr_spec.cr
+++ spec/gravatarcr_spec.cr
@@ -1,9 +1,30 @@
 require "./spec_helper"
+require "../src/gravatarcr.cr"
+
+include Gravatarcr
 
 describe Gravatarcr do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "should return url includes hashed email and given size" do
+    avatar_email = "pcjpcj2@gmail.com"
+    avatar_size = 64
+    url = gravatar_url(avatar_email, avatar_size)
+    url.should eq("http://gravatar.com/avatar/f43ebcb0ae804b58eec3fd06c2473804.png?s=64")
   end
+
+  it "should return 64 pixel gravatar when only user email passed" do
+    avatar_email = "pcjpcj2@gmail.com"
+    url = gravatar_url(avatar_email)
+    url.should eq("http://gravatar.com/avatar/f43ebcb0ae804b58eec3fd06c2473804.png?s=64")
+  end
+
+  it "should return default avatar when empty arguments" do
+    url = gravatar_url()
+    url.should eq("http://www.gravatar.com/avatar")
+  end
+
+  it "should return default avatar when pass only one Number" do
+    url = gravatar_url(64)
+    url.should eq("http://www.gravatar.com/avatar")
+  end
+
 end
--- src/gravatarcr.cr
+++ src/gravatarcr.cr
@@ -1,6 +1,19 @@
+require "digest"
 require "./gravatarcr/*"
 
-# TODO: Write documentation for `Gravatarcr`
 module Gravatarcr
-  # TODO: Put your code here
+  DEFAULT_SIZE = 64
+
+  def gravatar_url(email : String = "", size : Int32 = DEFAULT_SIZE)
+    if (email.size > 0 && size)
+      gravatar = Digest::MD5.hexdigest(email).downcase
+      url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+    else
+      "http://www.gravatar.com/avatar"
+    end
+  end
+
+  def gravatar_url(size : Int32 = DEFAULT_SIZE)
+    "http://www.gravatar.com/avatar"
+  end
 end
